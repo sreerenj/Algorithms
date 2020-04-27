6b1121b5db0773939dfc5244595a77b791d0f666
Sreerenj Gopimohanan <Sreerenj.Gopimohanan@ivanti.com>
2020-04-27 23:58:07 +0530

    Initial Commit

---
+++ CharCost/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CharCost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution("aabbcc", new int[]{1,2,1,2,1,2}));
+            Console.WriteLine(solution.solution("aaaaaaaaaaaaabbbbbbcccccddddd", new int[]{1,1,1,3,2,5,7,2,9,4,2,8,1,  6,3,4,2,8,1,  6,3,2,1,8,  3,2,9,4,2}));
+            Console.WriteLine(solution.solution("aaaab", new int[]{1,1,1,1,4}));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(string s, int[] c)
+        {
+            char[] chars = s.ToCharArray();
+            int currentLocation = 0;
+            int totalCost= 0;
+            while(currentLocation < s.Length-1)
+            {
+                // abccbd [0,1,2,3,4,5]
+                // aaaaaaaaaaaaabbbbbbcccccddddd
+                // aabbccdd [0,1,2,3,4,5,6,7]
+                if(chars[currentLocation] != chars[currentLocation + 1])
+                {
+                    currentLocation++;
+                    continue;
+                }
+                else
+                {
+                    int maxLocation = currentLocation; 
+                    int startLocation = currentLocation;
+                    
+                    while((currentLocation < s.Length -1) && (chars[currentLocation] == chars[currentLocation + 1]))
+                    {
+                        currentLocation++;
+                        if(currentLocation >= s.Length)
+                        {
+                            break;
+                        }
+                        if(c[currentLocation] > c[maxLocation])
+                        {
+                            maxLocation = currentLocation;
+                        }
+                    }
+                    int endLocation =  currentLocation;
+                    for(int i = startLocation; i <= endLocation; i++)
+                    {
+                        if(i != maxLocation)
+                        {
+                            totalCost+=c[i];
+                        }
+                    }
+
+
+                }
+
+            }
+            return totalCost;
+        }
+    }
+}
---
+++ KeyPair/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class GFG {
+	static public void Main () {
+	    int T = Convert.ToInt32(Console.ReadLine());
+	    for(int i = 0; i < T; i++)
+	    {
+	        string[] strNums = Console.ReadLine().Split(' ');
+	        int N = Convert.ToInt32(strNums[0]);
+	        int X = Convert.ToInt32(strNums[1]);
+	        
+	        string[] strNumbers = Console.ReadLine().Split(' ');
+	        int[] numbers = new int[N];
+	        HashSet<int> hashset = new HashSet<int>();
+	        bool isExist = false;
+	        for(int j = 0; j < N; j++)
+	        {
+	            numbers[j] = Convert.ToInt32(strNumbers[j]);
+	            int mod = numbers[j] % X;
+                Console.WriteLine(mod + " " + numbers[j]);
+	            if(hashset.Contains(mod))
+	            {
+	                isExist = true;
+	                break;
+	            }
+	            hashset.Add(numbers[j]);
+	        }   
+	        if(isExist)
+	        {
+	            Console.WriteLine("Yes");
+	        }
+	        else
+	        {
+	            Console.WriteLine("No");
+	        }
+	    }
+	}
+}
